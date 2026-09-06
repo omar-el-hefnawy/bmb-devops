d3c18707466de8394f527ea943a589582e89b162
omar-el-hefnawy <omar.moamen.elhefnawy@gmai.com>
2026-09-06 16:08:24 +0300

    Add Docker Files

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
-from app import sum_numbers, mult_numbers, sub_numbers
+from app import add_numbers, mult_numbers, sub_numbers
 
 def test_add_numbers():
-    assert sum_numbers(2, 3) == 5
-    assert sum_numbers(-1, 1) == 0
-    assert sum_numbers(0, 0) == 0
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
 
 def test_mult_numbers():
     assert mult_numbers(2, 3) == 6
